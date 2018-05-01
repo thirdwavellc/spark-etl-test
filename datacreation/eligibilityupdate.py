3c1f652c52c6c73a8e9aaca0d51ac38fa95bb689
Maxh1989 <mhansen1989@gmail.com>
2018-05-01 10:47:14 -0500

    eligibilityupdate and yaro.py changes

--- datacreation/eligibilityupdate.py
+++ datacreation/eligibilityupdate.py
@@ -23,23 +23,8 @@ from models.schemas.radice import eligibility_file
 
 
 
-def random_delete_entry(input_file):
-    spark = SparkSession\
-        .builder\
-        .appName("PySparkEligibiltyFile")\
-        .getOrCreate()
-
-    df = self.spark_session.read \
-                    .format("CSV") \
-                    .schema(eligibility_file) \
-                    .option("header", "false") \
-                    .option("delimiter", "|") \
-                    .option("treatEmptyValuesAsNulls", "true") \
-                    .load(input_file) \
-
-
-
-
+def random_delete_entry(df):
+        df.drop(df.index[index])
 
 
 def random_file_update(input_file):
@@ -47,13 +32,24 @@ def random_file_update(input_file):
      "address_line_1": fake.street_address(),}
     updated_attributes_array = ["last_name","first_name","email","city","state","zip_code","address_line_1"]
     df = pandas.DataFrame(pandas.read_csv("eligibility-sample.txt",sep="|",header=None,names=eligibility_file.fieldNames(),dtype=str))
+
+
     for index, row in df.iterrows():
         random_attribute = np.random.choice(updated_attributes_array)
         random_chance = random.randint(1, 100)
-        if random_chance<=100:
+        if random_chance<=10:
             df.loc[index,random_attribute]=updated_attributes[random_attribute]
 
+    #remove a random entries
 
+    for index, row in df.iterorw():
+        random_chance = random.randint(1,100)
+        random_entries = []
+        if random_chance < 1:
+            random_entries.append(index)
+
+    for random_entry in random_entries:
+        df.drop(df.index[random_entry], inplace=True)
 
 
     df.to_csv('transformed-test-data.txt',header=None,sep="|",index=False)
